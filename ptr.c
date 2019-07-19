b4d4c890e9b2048b5924c89556d28f1e37e39213
vanshika <vanshikachd@gmail.com>
2019-07-19 12:05:06 +0530

    traversing an array

---
+++ ptr.c
@@ -0,0 +1,17 @@
+include<stdio.h>
+int main()
+{
+int n,j;
+scanf("%d",&n);
+int arr[n];
+int *ptr=arr;
+for(int i=0;i<n;i++)
+{
+scanf("%d",*ptr+i;
+}
+
+for(int i=n;i>0;i--)
+{
+printf("%d",*ptr+i);
+}
+}
